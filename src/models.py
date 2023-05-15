85bb1cdbfdcb0c2f4a527a612fe076758eb9638b
a-lemus96 <alejandro.lemus@cimat.mx>
2023-05-15 12:11:00 -0600

    chore: Update variable names and documentation
    Perform general improvements for code readability.
    
    - Remove typing module dependency
    - Improve comments and fns documentation
    - Change some variable names

--- src/models.py
+++ src/models.py
@@ -1,6 +1,3 @@
-# std modules
-from typing import Tuple, List
-
 # third-party modules
 import torch
 from torch import nn
@@ -8,17 +5,16 @@ from torch import nn
 
 class PositionalEncoder(nn.Module):
     """Positional encoder module for mapping spatial coordinates and viewing
-    directions."""
-    def __init__(self,
-               d_input: int, 
-               n_freqs: int,
-               log_space: bool = False):
-        """Constructor method.
+    directions.
+    ----------------------------------------------------------------------------
+    """
+    def __init__(self, d_input, n_freqs, log_space=False):
+        """Constructor method. Builds a positional encoding
         ------------------------------------------------------------------------
         Args:
-            d_input: number of input dimensions
-            n_freqs: number of frequencies to be used in the mapping
-            log_space: compute frequencies in linear or log scale"""
+            d_input: int. number of input dimensions
+            n_freqs: int. number of frequencies to be used in the mapping
+            log_space: bool. compute frequencies in linear or log scale"""
         super().__init__()
         self.d_input = d_input
         self.n_freqs = n_freqs
@@ -40,74 +36,82 @@ class PositionalEncoder(nn.Module):
 
 
     # Forward method for passing inputs to the model
-    def forward(self, 
-                x) -> torch.Tensor:
-        '''Apply positional encoding to the input.
+    def forward(self, x):
+        """Applies positional encoding to the input coordinates.
+       -------------------------------------------------------------------------
         Args:
-            x: [N, dim_input]. N input coordinates.
+            x: (N, dim_input)-shape torch.Tensor. N input coordinates
         Returns:
-            y: [N, dim_input, 2 * n_freqs]. Embedded coordinates.
-        '''
+            y: (N, dim_input, 2*n_freqs)-shape torch.Tensor. Embedded coords"""
         return torch.concat([fn(x) for fn in self.embedding_fns], dim=-1)
 
 
 class NeRF(nn.Module):
-    '''Neural radiance field model.
-    '''
+    """Neural Radiance Field model definition.
+    ----------------------------------------------------------------------------
+    """
     def __init__(self, 
-                 d_input: int = 3,
-                 n_layers: int = 8,
-                 d_filter: int = 256,
-                 skip: Tuple[int] = (4,),
-                 d_viewdirs: Optional[int] = None):
+                 d_input=3,
+                 n_layers=8,
+                 d_filter=256,
+                 skip=(4,),
+                 d_viewdirs=None):
+        """Constructor method. Builds a fully connected network as that of
+        original NeRF paper using ReLU activation functions.
+        ------------------------------------------------------------------------
+        Args:
+            d_input: int. Dimension of spatial (with viewing dir) coords
+            n_layers: int. Number of hidden layers before applying bottleneck
+            d_filter: int. Width of hidden layers
+            skip: Tuple[int]. Layer positions at where to concatenate input
+            d_viewdirs: Optional[int]. Dimension of viewing direction coords""" 
         super().__init__()
         self.d_input = d_input
         self.skip = skip
-        self.activation = nn.functional.relu
+        self.activation = nn.functional.relu # use ReLU activation fn
         self.d_viewdirs = d_viewdirs
 
-        # Create model layers
-        self.layers = nn.ModuleList([nn.Linear(self.d_input, d_filter)] + 
-                                    [nn.Linear(d_filter + self.d_input, d_filter) if i in skip 
-                                     else nn.Linear(d_filter, d_filter) for i in range(n_layers - 1)])
+        # create model layers
+        hidden_layers = [nn.Linear(d_filter + self.d_input, d_filter) if i in skip 
+                         else nn.Linear(d_filter, d_filter) for i in range(n_layers - 1)]
+        self.layers = nn.ModuleList([nn.Linear(self.d_input, d_filter)] + hidden_layers)
 
-        # Create last layers
+        # create last layers
         if self.d_viewdirs is not None:
-            # If the field has view-dependent effects, split sigma and RGB
+            # field has view-dependent effects, split density and color
             self.sigma_out = nn.Linear(d_filter, 1)
-            self.rgb_filters = nn.Linear(d_filter, d_filter)
+            self.rgb_layer = nn.Linear(d_filter, d_filter)
             self.branch = nn.Linear(d_filter + self.d_viewdirs, d_filter // 2)
             self.output = nn.Linear(d_filter // 2, 3)
         else:
-            # If the vield has no view-dependency, use simpler output
+            # if the vield has no view-dependency, use simpler output
             self.output = nn.Linear(d_filter, 4)
 
-    def forward(self,
-                x: torch.Tensor,
-                viewdirs: Optional[torch.Tensor] = None) -> torch.Tensor:
-        '''Forward pass method with optional view directions argument.
+    def forward(self, x, viewdirs=None):
+        """Forward pass method with optional view directions argument.
+        ------------------------------------------------------------------------
         Args:
-            x: [N, 3]. Point coordinates.
-            viewdirs: [N, 3]. View directions
-        '''
-        # Check if view directions are not required but given as input
+            x: (N, 3)-shape torch.Tensor. Space coordinates
+            viewdirs: Optional[(N, 3)-shape torch.Tensor]. Viewing directions"""
+        # check if view directions are not required but given as input
         if self.d_viewdirs is None and viewdirs is not None:
-            raise ValueError('Model does not have view-dependent effects but viewing directions were given.')
+            err_msg = "Model does not have view-dependent effects but viewing directions were given."
+            raise ValueError(err_msg)
 
-        # Apply forward pass up to the layer just before the bottleneck
+        # apply forward pass up to the immediate layer before the bottleneck
         x_input = x
         for i, layer in enumerate(self.layers):
             x = self.activation(layer(x))
             if i in self.skip:
                 x = torch.cat([x, x_input], dim=-1)
         
-        # Apply bottleneck pass
+        # apply bottleneck pass
         if self.d_viewdirs is not None:
-            # Split sigma from network output
+            # get sigma from network output
             sigma = self.sigma_out(x)
 
-            # Obtain RGB value
-            x = self.rgb_filters(x)
+            # get RGB value
+            x = self.rgb_layer(x)
             x = torch.concat([x, viewdirs], dim=-1)
             x = self.activation(self.branch(x)) 
             x = self.output(x)
