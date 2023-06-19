7ea17a60a3c44d40ac0db0fab6dd4c4211c4f258
a-lemus96 <alejandro.lemus@cimat.mx>
2023-06-19 14:52:30 -0600

    feat: Upgrade dataset class definition
    Remove `NeRFDataset` and `DSNeRFDataset` class definitions and add a new
    class `RealisticSynthetic` to load bounded scenes. From now on, a
    specific class definition will be created per Dataset type.

--- src/dataset.py
+++ src/dataset.py
@@ -1,160 +1,144 @@
 # standard library modules
+import json
 import os
 from typing import Tuple, List, Union, Callable
 
 # third-party modules
+import imageio as iio
+import numpy as np
 import torch
+from torch import Tensor
 from torch.utils.data import Dataset
+from torchvision.transforms import Resize
 
 # custom modules
-from dataload import *
-from utilities import *
+import utilities as utils
 
-# NERF DATASET
+class SyntheticRealistic(Dataset):
+    """
+    Synthetic realistic dataset. It is made up of N x H x W ray origins and
+    directions in world coordinate frame paired with ground truth pixel colors.
+    Here, N is the number of training images of size H x W.
+    ----------------------------------------------------------------------------
+    """
+    def __init__(self, scene: str, root: str, factor: float=None) -> None:
+        """
+        Initialize the dataset.
+        ------------------------------------------------------------------------
+        Args:
+            scene (str): scene name
+            root (str): root directory. It can be either 'train', 'val', or 
+                        'test'
+            factor (float): factor to scale images and camera intrinsics
+        Returns:
+            None
+        """
+        super(SyntheticRealistic).__init__()  # inherit from Dataset
+        self.scene = scene
+        self.root = root
+        self.factor = factor
 
-class NerfDataset(Dataset):
-    r"""NeRF dataset. A NeRF dataset consists of N x H x W ray origins and
-    directions relative to the world frame. Here, N is the number of training
-    images of size H x W."""
-    def __init__(
-        self,
-        dataset: str,
-        subset: str,
-        scene: str,
-        n_imgs: int,
-        test_idx: int,
-        f_forward: bool = False,
-        factor: int = None,
-        near: int = 2.,
-        far: int = 7.):
+        # load the dataset
+        imgs, poses, hwf = self.__load()
 
-        # Initialize attributes
-        self.n_imgs = n_imgs
-        self.test_idx = test_idx
-        self.near = near # near and far sampling bounds for each ray
-        self.far = far
+        # compute rays
+        H, W, f = hwf
+        rays = torch.stack([torch.cat(utils.get_rays(H, W, f, p), -1) 
+                            for p in poses], 0)
+        rays = rays.reshape(-1, 6)
+        self.rays_o = rays[:, :3]
+        self.rays_d = rays[:, 3:]
 
-        # Load images and camera poses
-        reader = DataReader(dataset, subset, scene, factor)
-        imgs, poses, hwf, _, _ = reader.get_data()
+        # add pixel colors
+        self.rgba = imgs.reshape(-1, 4)
 
-        # Validation image
-        self.testimg = imgs[test_idx]
-        self.testpose = poses[test_idx]
+    def __len__(self) -> int:
+        """Compute the number of training samples.
+        ------------------------------------------------------------------------
+        Args:
+            None
+        Returns:
+            N (int): number of training samples
+        """
+        return self.rgb.shape[0]
+    def __getitem__(self, idx: int) -> Tuple[Tensor, Tensor, Tensor]:
+        """Get a training sample by index.
+        ------------------------------------------------------------------------
+        Args:
+            idx (int): index of the training sample
+        Returns:
+            ray_o (Tensor): [3,]. Ray origin
+            ray_d (Tensor): [3,]. Ray direction
+            rgba (Tensor): [4,]. Pixel RGBa color
+        """
+        return self.rays_o[idx], self.rays_d[idx], self.rgba[idx]
 
-        # Selection indices
-        self.inds = torch.arange(n_imgs)
 
-        if f_forward:
-            # Set central view arbitrarily
-            central = 15
-            # Retrieve camera origins
-            origins = poses[:, :3, 3]
-            # Compute camera distances to central camera
-            dists = torch.sum((origins[central] - origins)**2, -1)
-            # Sort distances and retrieve indices
-            _, select = torch.sort(dists)
+    def __factor(self, imgs: Tensor, hwf: Tensor) -> Tuple[Tensor, Tensor]:
+        """
+        Downsample images and apply resize factor to camera intrinsics.
+        ------------------------------------------------------------------------
+        Args:
+            imgs: [N, H, W, 4]. RGBa images
+            hwf: [3,]. Camera intrinsics.
+        Returns:
+            new_imgs: [N, H, W, 4]. Downsampled images
+            new_hwf: [3,]. Updated camera intrinsics
+        """
+        # apply factor to camera intrinsics
+        H, W, f = hwf
+        new_W, new_H = H // factor, W // factor
+        new_focal = hwf[2] / float(factor)
+        new_hwf = torch.Tensor((new_H, new_W, new_focal))
 
-            # Select validation image and pose
-            self.test_idx = select[n_imgs]
-            self.testimg = imgs[self.test_idx]
-            self.testpose = poses[self.test_idx]
+        # downsample images
+        new_imgs = Resize((new_H, new_W))(imgs)
 
-            # Select training images and poses
-            self.inds = select[:n_imgs]
-            imgs = imgs[self.inds]
-            poses = poses[self.inds]
+        return new_imgs, new_hwf
 
-        H, W, self.focal = hwf
-        self.H, self.W = int(H), int(W)
-        N = self.inds.shape[0]
-        
-        # Get rays
-        self.rays = torch.stack([torch.stack(
-                                 get_rays(self.H, self.W, self.focal, p), 0)
-                                 for p in poses[:N]], 0)
+    def __load(self) -> Tuple[Tensor, Tensor, Tensor]:
+        """
+        Load the dataset.
+        ------------------------------------------------------------------------
+        Args:
+            None
+        Returns:
+            imgs: [N, H, W, 4]. RGBa images
+            poses: [N, 4, 4]. Camera poses
+            hwf: [3,]. Camera intrinsics. h stands for image height, w for image
+                 width and f for focal length
+        """
+        scene = self.scene
+        root = self.root
+        path = os.path.join('..', 'data', 'synthetic', scene)
+        # load JSON file
+        with open(os.path.join(path, f'transforms_{root}.json'), 'r') as f:
+            meta = json.load(f) # metadata
 
-        # Append RGB supervision and local rays dirs info
-        rays_rgb = torch.cat([self.rays,
-                              imgs[:N, None]], 1) 
+        # load images and camera poses
+        imgs = []
+        poses = []
+        for frame in meta['frames']:
+            fname = os.path.join(path, frame['file_path'] + '.png')
+            imgs.append(iio.imread(fname))
+            poses.append(np.array(frame['transform_matrix']))
 
-        # Rearrange data and reshape
-        rays_rgb = torch.permute(rays_rgb, [0, 2, 3, 1, 4])
-        rays_rgb = rays_rgb.reshape([-1, rays_rgb.shape[3], 3])
-        rays_rgb = torch.transpose(rays_rgb, 0, 1)
-        
-        self.rays_rgb = rays_rgb.type(torch.float32)
+        # convert to numpy arrays
+        imgs = (np.stack(imgs, axis=0) / 255.).astype(np.float32)
+        poses = np.stack(poses, axis=0).astype(np.float32)
 
-    def __len__(self):
-        return self.rays_rgb.shape[1]
+        # compute image height, width and camera's focal length
+        H, W = imgs.shape[1:3]
+        fov_x = meta['camera_angle_x'] # field of view along camera x-axis
+        focal = 0.5 * W / np.tan(0.5 * fov_x)
+        hwf = np.array([H, W, np.array(focal)])
 
-    def __getitem__(self, idx):
-        rays_o, rays_d, target_pixs = self.rays_rgb[:, idx]
-        
-        return rays_o, rays_d, target_pixs
+        imgs = torch.from_numpy(imgs)
+        poses = torch.from_numpy(poses)
+        hwf = torch.from_numpy(hwf)
 
-# DEPTH SUPERVISED NERF DATASET
+        # scale images and camera intrinsics if applicable
+        if self.factor is not None:
+            imgs, hwf = self.__scale(imgs, hwf)
 
-class DSNerfDataset(NerfDataset):
-    r"""Depth supervised NeRF dataset. It consists of N x H x W ray origins and
-    directions relative to the world frame. Here, N is the number of training
-    images of size H x W. It also contains depth information associated to each
-    ray."""
-    def __init__(
-        self,
-        dataset: str,
-        subset: str,
-        scene: str,
-        n_imgs: int,
-        test_idx: int,
-        f_forward: bool = False,
-        factor: int = None,
-        near: int=2.,
-        far: int=7.): 
-        # Call base class constructor method
-        super().__init__(dataset, subset, scene, n_imgs, test_idx,
-                         f_forward, factor, near, far)
-
-        # Load images, camera poses and depth maps
-        reader = DataReader(dataset, subset, scene, factor)
-        imgs, poses, hwf, maps, backs = reader.get_data()
-
-        self.test_back = backs[self.test_idx].type(torch.bool)
-        self.test_map = maps[self.test_idx]
-        backs = backs[self.inds]
-        maps = maps[self.inds]
-        N = backs.shape[0]
-        print(torch.min(maps), torch.max(maps))
- 
-        # Local rays
-        #self.local_dirs = get_rays(self.H, self.W, self.focal, local_only=True)
-
-        # Compute depth along rays for test depth map
-        #test_map = -test_map / self.local_dirs[..., -1]
-        #self.test_map = test_map.type(torch.float32) * (~test_back)
-
-        # Expanded version of local rays according to number of training imgs
-        #local_dirs = self.local_dirs[None, None, ...].expand(N, 1, self.H,
-        #                                                     self.W, 3)
-
-        # Compute depth along rays
-        #t_maps = -maps[:, None, ...] / local_dirs[..., -1]
-
-        # Compute background masks and concatenate with depth maps
-        #backs = backs[:, None, ..., None]
-        #maps = maps[:, None, ..., None]
-        print(maps.shape, backs.shape)
-        #depths = torch.cat((maps, backs), 1)
-
-        #depths = torch.permute(depths, [0, 2, 3, 1, 4])
-        #depths = depths.reshape([-1, depths.shape[3], 1]).type(torch.float32)
-        #depths = torch.transpose(depths, 0, 1)
-        self.depths = maps.reshape(maps.numel()).type(torch.float32)
-        self.backs = backs.reshape(backs.numel()).type(torch.bool)
-
-    def __getitem__(self, idx):
-        rays_o, rays_d, target_pixs = self.rays_rgb[:, idx]
-        depths = self.depths[idx]
-        backs = self.backs[idx]
-
-        return rays_o, rays_d, target_pixs, depths, backs
+        return imgs, poses, hwf
