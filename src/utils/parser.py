553ec2d59b2844e837a412061f5c86a14e42951b
a-lemus96 <alejandro.lemus@cimat.mx>
2023-10-03 14:32:25 -0600

    [factor]: Reorganize args definition layout
    Limit some arg entry values and restructure code layout.

--- src/utils/parser.py
+++ src/utils/parser.py
@@ -5,88 +5,153 @@ def config_parser() -> argparse.Namespace:
     """Creates a parser for command-line arguments.
     ----------------------------------------------------------------------------
     """
-    parser = argparse.ArgumentParser(description='Train NeRF for view synthesis.')
+    parser = argparse.ArgumentParser(
+            description='Train NeRF for view synthesis.'
+    )
 
-    # encoder(s)
-    parser.add_argument('--d_input', dest='d_input', default=3, type=int,
-                        help='Spatial input dimension')
-    parser.add_argument('--n_freqs', dest='n_freqs', default=10, type=int,
-                        help='Number of encoding functions for spatial coords')
-    parser.add_argument('--log_space', dest='log_space', action="store_false",
-                        help='If not set, frecuency scale in log space')
-    parser.add_argument('--no_dirs', dest='no_dirs', action="store_true",
-                        help='If set, do not model view-dependent effects')
-    parser.add_argument('--n_freqs_views', dest='n_freqs_views', default=4,
-                        type=int, help='Number of encoding fns for view dirs')
-
-    # model
-    parser.add_argument('--model', dest='model', default='nerf', type=str,
-                        help='Model to be used for training')
-    parser.add_argument('--d_filter', dest='d_filter', default=256, type=int,
-                        help='Linear layer filter dimension')
-    parser.add_argument('--n_layers', dest='n_layers', default=8, type=int,
-                        help='Number of layers preceding bottleneck')
-    parser.add_argument('--skip', dest='skip', default=[4], type=list,
-                        help='Layers at which to apply input residual')
-
-    # dataset
-    parser.add_argument('--dataset', dest='dataset', default='synthetic', 
-                        type=str, help="Dataset to choose scenes from")
-    parser.add_argument('--scene', dest='scene', default='lego', type=str,
-                        help="Scene to be used for training")
-    parser.add_argument('--n_imgs', dest='n_imgs', default=None, type=int,
-                        help="Number of images to be used for training")
-    parser.add_argument('--white_bkgd', dest='white_bkgd', action="store_true",
-                        help="Use white backgroung for training imgs")
-    parser.add_argument('--img_mode', dest='img_mode', action="store_true",
-                        help="If set, iterate over images instead of rays")
-
-    # training
-    parser.add_argument('--n_iters', dest='n_iters', default=10**5, type=int,
-                        help='Number of training iterations')
-    parser.add_argument('--warmup_iters', dest='warmup_iters', default=2500,
-                        type=int, help='Number of iterations for warmup phase')
-    parser.add_argument('--batch_size', dest='batch_size', default=2**12, type=int,
-                        help='Number of rays per optimization step')
-    parser.add_argument('--lrate', dest='lrate', default=5e-4, type=float,
-                        help='Learning rate')
-    parser.add_argument('--scheduler', dest='scheduler', default='mip', type=str,
-                        help='Learning rate scheduler')
-    parser.add_argument('--device_num', dest='device_num', default=1, type=int,
-                        help="Number of CUDA device to be used for training")
-
-    # validation
-    parser.add_argument('--val_rate', dest='val_rate', default=500, type=int,
-                        help='Number of iterations between validation steps')
-    parser.add_argument('--val_ratio', dest='val_ratio', 
-                        default=0.25, type=float,
-                        help='Ratio of val data to be used in between epochs')
-
-    # scale-space
-    parser.add_argument('--scale_space', dest='scale_space', action="store_true",
-                        help='If set, use scale-space for training')
-
-    # depth supervision
-    parser.add_argument('--use_bkgd', dest='use_bkgd', action="store_true",
-                        help='If set, use background pixels for depth supervision')
-    parser.add_argument('--mu', dest='mu', default=None, type=float,
-                        help='Balancing hyperparameter for depth loss')
+    #-------------------------------encoder------------------------------------#
 
 
-    # directories
-    parser.add_argument('--out_dir', dest='out_dir', default="../out/",
-                        type=str, help="Base directory for storing results")
+    parser.add_argument(
+            '--d_input', default=3, type=int,
+            help='Spatial input dimension'
+    )
+    parser.add_argument(
+            '--n_freqs', default=10, type=int,
+            help='Number of encoding functions for spatial coords'
+    )
+    parser.add_argument(
+            '--log_space', action="store_false",
+            help='If not set, frecuency scale in log space'
+    )
+    parser.add_argument(
+            '--no_dirs', action="store_true",
+            help='If set, do not model view-dependent effects'
+    )
+    parser.add_argument(
+            '--n_freqs_views', dest='n_freqs_views', default=4,
+            type=int, help='Number of encoding fns for view dirs'
+    )
 
-    # video rendering
-    parser.add_argument('--render_only', dest='render_only', action='store_true',
-                        help='If set, load pretrained model to render video')
+    #--------------------------------model-------------------------------------#
 
-    # debugging
-    parser.add_argument('--debug', dest='debug', action='store_true',
-                        help='If set, run in debug mode')
-    parser.add_argument('--sweep', dest='sweep', action='store_true',
-                        help='If set, run in W&B sweep mode')
-            
+    parser.add_argument(
+            '--model', choices=['nerf', 'freenerf', 'sinerf'], default='nerf',
+            help='Model to be used for training'
+    )
+    parser.add_argument(
+            '--d_filter', default=256, type=int,
+            help='Linear layer filter dimension'
+    )
+    parser.add_argument(
+            '--n_layers', default=8, type=int,
+            help='Number of layers preceding bottleneck'
+    )
+    parser.add_argument(
+            '--skip', default=[4], type=list,
+            help='Layers at which to apply input residual'
+    )
+
+    #---------------------------------data-------------------------------------#
+
+    parser.add_argument(
+            '--dataset', default='synthetic', 
+            type=str, help="Dataset to choose scenes from"
+    )
+    parser.add_argument(
+            '--scene' default='lego', type=str,
+            help="Scene to be used for training"
+    )
+    parser.add_argument(
+            '--n_imgs', default=100, type=int,
+            help="Number of images to be used for training"
+    )
+    parser.add_argument(
+            '--white_bkgd', action="store_true",
+            help="Use white backgroung for training imgs"
+    )
+    parser.add_argument(
+            '--img_mode', action="store_true",
+            help="If set, iterate over images instead of rays")
+
+    #-------------------------------training-----------------------------------#
+
+    parser.add_argument(
+            '--n_iters', default=20**3, type=int,
+            help='Number of training iterations'
+    )
+    parser.add_argument(
+            '--warmup_iters', default=2500, type=int, 
+            help="Number of iterations for mip scheduler's warmup phase"
+    )
+    parser.add_argument(
+            '--batch_size', default=2**12, type=int,
+            help='Number of rays per optimization step'
+    )
+    parser.add_argument(
+            '--lrate', default=5e-4, type=float,
+            help='Learning rate'
+    )
+    parser.add_argument(
+            '--scheduler', choices=['exp', 'mip'], default='exp',
+            help='Learning rate scheduler'
+    )
+    parser.add_argument(
+            '--device_num', default=1, type=int,
+            help="Number of CUDA device to be used for training"
+    )
+
+    #-------------------------------validation---------------------------------#
+
+    parser.add_argument(
+            '--val_rate', default=500, type=int,
+            help='Number of iterations between validation steps'
+    )
+    parser.add_argument(
+            '--val_ratio', default=0.25, type=float,
+            help='Ratio of val data to be used in between epochs'
+    )
+
+    #--------------------------------scale-------------------------------------#
+
+    parser.add_argument(
+            '--scale_space', action="store_true",
+            help='If set, use scale-space for training'
+    )
+
+    #---------------------------------depth------------------------------------#
+
+    parser.add_argument(
+            '--use_bkgd', action="store_true",
+            help='If set, use background pixels for depth supervision'
+    )
+    parser.add_argument(
+            '--mu', default=None, type=float,
+            help='Balancing hyperparameter for depth loss'
+    )
+
+    #-------------------------------logging------------------------------------#
+
+    parser.add_argument(
+            '--out_dir', default="../out/",
+            type=str, help="Base directory for storing results")
+
+    #-------------------------------debugging----------------------------------#
+
+    parser.add_argument(
+            '--debug', action='store_true',
+            help='If set, run in debug mode'
+    )
+    parser.add_argument('--sweep', action='store_true',
+            help='If set, run in W&B sweep mode'
+    )
+
+    #----------------------------videorendering--------------------------------#
+
+    parser.add_argument(
+            '--render_only', action='store_true',
+            help='If set, load pretrained model to render video'
+    )
 
     args = parser.parse_args()
 
