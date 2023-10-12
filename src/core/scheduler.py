5d710bb4bc0537ca32d5141867cad46a2de1d133
a-lemus96 <alejandro.lemus@cimat.mx>
2023-10-12 15:35:42 -0600

    [feat]: Add RootP lr scheduler
    Add new scheduler (p-root-based) and refactor scheduler.py module so
    that there is now a parent class called Scheduler from which the rest of
    the schedulers inherit their attributes and methods.

--- src/core/scheduler.py
+++ src/core/scheduler.py
@@ -3,15 +3,16 @@ import numpy as np
 import torch
 from torch.optim import Optimizer
 
-class ExponentialDecay:
-    """Exponentially decay the learning rate"""
+class Scheduler:
+    """Learning rate scheduler."""
     def __init__(
             self,
             optim: Optimizer,
             steps: int,
             lr_range: tuple = (5e-4, 5e-5)
     ) -> None:
-        """Initialize the scheduler.
+        """
+        Initialize the scheduler.
         ------------------------------------------------------------------------
         Args:
             optim (Optimizer): The optimizer to use
@@ -29,16 +30,57 @@ class ExponentialDecay:
         self.current_step = 0
 
     def step(self) -> None:
-        """Update the learning rate."""
+        """
+        Update optimizer learning rate
+        ------------------------------------------------------------------------
+        """
         self.current_step += 1
         for param_group in self.optim.param_groups:
             param_group["lr"] = self.lr
 
+class ExponentialDecay(Scheduler):
+    """
+    Exponential decay for the learning rate
+    ----------------------------------------------------------------------------
+    """
     @property
     def lr(self) -> float:
-        """Calculate the learning rate."""
+        """Compute the learning rate."""
         decay_rate = -np.log(self.lr_min / self.lr_max) / self.steps
         return self.lr_max * np.exp(-decay_rate * self.current_step)
+
+class RootP:
+    """p-root-based learning rate decay."""
+    def __init__(
+            self,
+            optim: Optimizer,
+            steps: int,
+            lr_range: tuple = (5e-4, 5e-5),
+            p: int = 2
+    ) -> None:
+        """
+        Initialize the scheduler.
+        ------------------------------------------------------------------------
+        Args:
+            optim (Optimizer): The optimizer to use
+            steps (int): The number of steps to decay the learning rate over
+            lr_range (tuple): The range of learning rates to iterate between
+            p (int): The p-root to use
+        Returns:
+            None
+        ------------------------------------------------------------------------
+        """
+        super().__init__(optim, steps, lr_range)
+        if p == 0:
+            raise ValueError("p must be different from 0.")
+        self.p = int(p)
+
+    @property
+    def lr(self) -> float:
+        """Compute the learning rate."""
+        p, N, k = self.p, self.steps, self.current_step
+        t = (((1. - 0.5 ** p) / N) * k + 0.5 ** p) ** (1. / p)
+        return 2 * (self.lr_max - self.lr_min) * (1. - min(1., t)) + self.lr_min
 
 class MipNerf:
     """MipNerf learning rate scheduler."""
@@ -63,20 +105,10 @@ class MipNerf:
             None
         ------------------------------------------------------------------------
         """
-        self.optim = optim
-        self.lr_max, self.lr_min = lr_range
-        if self.lr_max <= self.lr_min:
-            raise ValueError("lr_max must be greater than lr_min.")
-        self.steps = steps
+        super().__init__(optim, steps, lr_range)
         self.warmup_steps = warmup_steps
         self.current_step = 0
         self.scale = scale
-
-    def step(self) -> None:
-        """Update the learning rate."""
-        self.current_step += 1
-        for param_group in self.optim.param_groups:
-            param_group["lr"] = self.lr
 
     @property
     def lr(self) -> float:
