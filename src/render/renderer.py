efc5144df74f966158704b6abe06e9c7d1ef99f4
a-lemus96 <alejandro.lemus@cimat.mx>
2024-02-19 17:02:38 -0600

    [feat]: Create `renderer.py` class
    This commit sketches the class design discussed in #30. Functions are
    yet to be implemented and skeleton is subject to further changes.

---
+++ src/render/renderer.py
@@ -0,0 +1,28 @@
+class Renderer:
+    def __init__(self):
+        pass
+
+    def render_rays(self):
+        pass
+
+    def render_poses(self):
+        pass
+
+    def init_estimator(self):
+        pass
+
+    def step(self):
+        pass
+
+    @staticmethod
+    def _sigma_fn():
+        pass
+
+    @staticmethod
+    def _rgb_sigma_fn():
+        pass
+
+    @staticmethod
+    def _occ_eval_fn():
+        pass
+
