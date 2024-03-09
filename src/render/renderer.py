[fix]: Loss not tracking gradient
This commit resolves #69:

- Bypass rendering step in render_rays fn for empty idxs
- Bypass training loop when rendering_output is None

src/render/renderer.py
@@ -16,7 +16,7 @@ class Renderer:
             far: float,
             chunksize: int,
             white_bkgd: bool = True,
-            train_mode: bool = False,
+            training: bool = False,
             **kwargs
     ):
         """
@@ -27,7 +27,7 @@ class Renderer:
             - far: float. Far bound.
             - chunksize: int. Chunk size for rendering.
             - white_bkgd: bool. Whether to use a white background.
-            - train_mode: bool. Whether to use the renderer in training mode.
+            - training: bool. Whether to use the renderer in training mode.
             - **kwargs: Dict. Additional arguments for the estimator.
         ------------------------------------------------------------------------
         """
@@ -40,7 +40,7 @@ class Renderer:
         dark = torch.zeros(3, dtype=torch.float32)
         self.bkgd = light if white_bkgd else dark
         # set operational mode
-        self.train_mode = train_mode
+        self.training = training
         # unpack kwargs
         self.render_step_size = kwargs['render_step_size']
         aabb = kwargs['aabb']
@@ -86,30 +86,27 @@ class Renderer:
                 t_min=self.tn,
                 t_max=self.tf,
                 render_step_size=self.render_step_size,
-                stratified=self.train_mode,
+                stratified=self.training,
         )
+        # check if no rays intersected the grid
+        if ray_idxs.shape[0] == 0:
+            return None
 
         # query local rgb and density
         def _rgb_sigma_fn(t_starts, t_ends, ray_idxs):
-                to = rays_o[ray_idxs]
-                td = rays_d[ray_idxs]
-                x = to + td * (t_starts + t_ends)[:, None] / 2.0
-                out = model(x, td)
-                rgbs = out[..., :3]
-                sigmas = out[..., -1]
+            to = rays_o[ray_idxs]
+            td = rays_d[ray_idxs]
+            x = to + td * (t_starts + t_ends)[:, None] / 2.0
+            out = model(x, td)
+            rgbs = out[..., :3]
+            sigmas = out[..., -1]
 
-                return rgbs, sigmas.squeeze(-1)
+            return rgbs, sigmas.squeeze(-1)
 
+        pdb.set_trace()
         # perform volume rendering
-        try:
-            data = rendering(t_starts, t_ends, ray_idxs, n_rays=len(rays_o),
-                    rgb_sigma_fn=_rgb_sigma_fn, render_bkgd=self.bkgd)
-        except AssertionError as assert_err:
-            print(assert_err)
-            data = (self.bkgd.expand(rays_o.shape), 
-                    torch.zeros_like(rays_o),
-                    torch.zeros_like(rays_o[:-1]).unsqueeze(-1), 
-                    {})
+        data = rendering(t_starts, t_ends, ray_idxs, n_rays=len(rays_o),
+                rgb_sigma_fn=_rgb_sigma_fn, render_bkgd=self.bkgd)
         
         return data
         
@@ -178,11 +175,11 @@ class Renderer:
                 occ_eval_fn=_occ_eval_fn, occ_thre=self.occ_thre)
 
     def eval(self):
-        self.train_mode = False
+        self.training = False
         self.estimator.eval()
 
     def train(self):
-        self.train_mode = True
+        self.training = True
         self.estimator.train()
 
     def set_chunksize(self, value):

src/run-nerf.py
@@ -15,6 +15,7 @@ import numpy as np
 import plotly.graph_objects as go
 from skimage.metrics import structural_similarity as SSIM
 import torch
+import pdb
 from torch import nn
 import torch.nn.functional as F
 from torch.optim import Optimizer
@@ -234,7 +235,10 @@ def train(
         # render rays
         rays_o, rays_d = rays_o.to(device), rays_d.to(device)
         render_output = renderer.render_rays(rays_o, rays_d, model)
-        rgb, _, depth, _ = render_output
+        # skip if no valid rendering output
+        if render_output is None:
+            continue
+        rgb, _, depth, _ = render_output    # unpack rendering output
         
         # compute loss and PSNR
         rgb_gt = rgb_gt.to(device)