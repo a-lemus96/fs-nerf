28b4ea83e8a31ccf3559a2d65847fbd2335f63f4
a-lemus96 <alejandro.lemus@cimat.mx>
2024-04-02 16:26:39 -0600

    [feat]: Include `type` parameter for OccReg
    Implement linear and exponential decay regularizer. All constants a, b
    must satisfy a, b > 0.

--- src/core/loss.py
+++ src/core/loss.py
@@ -8,13 +8,14 @@ class OcclusionRegularizer():
     Occlussion regularizer to penalize dense fields near the camera.
     ----------------------------------------------------------------------------
     """
-    def __init__(self, a: float, b: float):
+    def __init__(self, a: float, b: float, type: str = 'linear'):
         """
         Initializes the occlusion regularizer.
         ------------------------------------------------------------------------
         Args:
             a (float): bias of the regularizer
             b (float): factor of the regularizer
+            type (str): type of the occlusion regularizer
         """
         self.a = a
         self.b = b
@@ -30,5 +31,23 @@ class OcclusionRegularizer():
         Returns:
             Tensor: occlusion regularization term of shape (B)
         """
-        occlusion = (-a * t_vals + b) * sigmas
+        occlusion = self._weights(t_vals) * sigmas
         return occlusion.sum(dim=-1)
+
+    def _weights(self, t_vals: Tensor) -> Tensor:
+        """
+        Computes importance weights for occlusion regularization.
+        ------------------------------------------------------------------------
+        Args:
+            t_vals (Tensor): depth values of shape (B, N)
+        Returns:
+            Tensor: importance weights of shape (B, N)
+        """
+        match self.type:
+            case 'linear':
+                weights = -self.a * t_vals + self.b
+            case 'exp':
+                weights = self.a * torch.exp(-self.b * t_vals)
+            case _:
+                raise ValueError(f'Unknown occlusion regularizer type: {self.type}')
+        return weights
