85c11aa086e60263c4f193f46cea7bf444e91b13
a-lemus96 <alejandro.lemus@cimat.mx>
2026-03-08 17:43:32 -0600

    fix: duplicated metrics logging

--- src/playground/model_trainers/nerf_trainer.py
+++ src/playground/model_trainers/nerf_trainer.py
@@ -155,16 +155,17 @@ class NeRFModelTrainer(ModelTrainerBase):
                 loss += alpha * freq_reg
 
             # occlusion regularization
+            metrics = {"train_psnr": psnr, "lr": self.lr_scheduler.lr, "alpha": alpha}
             if self.occl_regularizer is not None and result.weights is not None and result.weights.numel() > 0:
                 occl_loss = self.occl_regularizer(result)
                 loss += self.occl_beta * occl_loss
                 if not self.debug_mode:
-                    wandb.log({"occl_loss": occl_loss.item()})
+                    metrics["occl_loss"] = occl_loss.item()
 
             self.__training_step(k, model, loss)
 
             if not self.debug_mode:
-                wandb.log({"train_psnr": psnr, "lr": self.lr_scheduler.lr, "alpha": alpha})
+                wandb.log(metrics)
 
         return
 
