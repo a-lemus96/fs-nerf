f08b082d00ec7adab49ed578ce3cebd9587a38b4
a-lemus96 <alejandro.lemus@cimat.mx>
2026-03-16 14:33:04 -0600

    refactor parser to add argument groups

--- src/utils/parser.py
+++ src/utils/parser.py
@@ -8,28 +8,25 @@ def config_parser() -> argparse.Namespace:
     """
     parser = argparse.ArgumentParser(description="Train NeRF for view synthesis.")
 
-    # -------------------------------encoder------------------------------------#
-
-    parser.add_argument(
-        "--d_input", default=3, type=int, help="Spatial input dimension"
-    )
-    parser.add_argument(
+    enc = parser.add_argument_group("Encoder")
+    enc.add_argument("--d_input", default=3, type=int, help="Spatial input dimension")
+    enc.add_argument(
         "--n_freqs",
         default=10,
         type=int,
-        help="Number of encoding functions for spatial coords",
+        help="Number of encoding functions for spatial coordinates",
     )
-    parser.add_argument(
+    enc.add_argument(
         "--log_space",
         action="store_false",
-        help="If not set, frecuency scale in log space",
+        help="If not set, frequency scale is in logarithmic space",
     )
-    parser.add_argument(
+    enc.add_argument(
         "--no_dirs",
         action="store_true",
         help="If set, do not model view-dependent effects",
     )
-    parser.add_argument(
+    enc.add_argument(
         "--n_freqs_views",
         dest="n_freqs_views",
         default=4,
@@ -37,189 +34,177 @@ def config_parser() -> argparse.Namespace:
         help="Number of encoding fns for view dirs",
     )
 
-    # --------------------------------model-------------------------------------#
-
-    parser.add_argument(
+    model = parser.add_argument_group("Model")
+    model.add_argument(
         "--model",
         choices=["nerf", "sinerf", "ensemble", "sires"],
         default="nerf",
         help="Model to be used for training",
     )
-    parser.add_argument(
+    model.add_argument(
         "--d_filter", default=256, type=int, help="Linear layer filter dimension"
     )
-    parser.add_argument(
+    model.add_argument(
         "--n_layers", default=8, type=int, help="Number of layers preceding bottleneck"
     )
-    parser.add_argument(
+    model.add_argument(
         "--skip",
         default=[4],
         type=list,
-        help="Layers at which to apply input residual for NeRF",
+        help="Layers at which to apply input residual connections",
     )
 
-    # ---------------------------------data-------------------------------------#
-
-    parser.add_argument(
+    data = parser.add_argument_group("Data")
+    data.add_argument(
         "--dataset",
         choices=["synthetic", "llff"],
-        default="synthetic",
+        default="llff",
         type=str,
         help="Dataset to choose scenes from",
     )
-    parser.add_argument(
-        "--scene", default="lego", type=str, help="Scene to be used for training"
+    data.add_argument(
+        "--scene", default="flower", type=str, help="Scene to be used for training"
     )
-    parser.add_argument(
+    data.add_argument(
         "--n_imgs",
         default=100,
         type=int,
         help="Number of images to be used for training",
     )
-    parser.add_argument(
+    data.add_argument(
         "--img_mode",
         action="store_true",
         help="If set, iterate over images instead of rays for training",
     )
-
-    # args for synthetic dataset
-    parser.add_argument(
+    data.add_argument(
         "--white_bkgd",
         action="store_true",
         help="Use white background for training imgs",
     )
-
-    # args for llff dataset
-    parser.add_argument(
+    data.add_argument(
         "--factor", default=4, type=int, help="Downsample factor for LLFF dataset"
     )
-    parser.add_argument(
+    data.add_argument(
         "--bd_factor", default=0.75, type=float, help="Bound factor for LLFF dataset"
     )
-    parser.add_argument(
+    data.add_argument(
         "--no_recenter",
         action="store_true",
         help="If set, do not recenter LLFF dataset",
     )
 
-    # -------------------------------training-----------------------------------#
-
-    parser.add_argument(
+    train = parser.add_argument_group("Training")
+    train.add_argument(
         "--n_iters", default=20**3, type=int, help="Number of training iterations"
     )
-    parser.add_argument(
+    train.add_argument(
         "--batch_size",
         default=1024,
         type=int,
         help="Number of rays per optimization step",
     )
-    parser.add_argument(
+    train.add_argument(
         "--lro", default=5e-4, type=float, help="Initial learning rate for optimizer"
     )
-    parser.add_argument(
+    train.add_argument(
         "--lrf", default=0.0, type=float, help="Final learning rate for optimizer"
     )
-    parser.add_argument(
+    train.add_argument(
         "--decay_rate",
         default=0.1,
         type=float,
         help="Decay rate for exponential learning rate scheduler",
     )
-    parser.add_argument(
+    train.add_argument(
         "--Td",
         default=250000,
         type=int,
         help="Number of iterations for learning rate decay",
     )
-    parser.add_argument(
+    train.add_argument(
         "--scheduler",
         choices=["const", "exp"],
         default="exp",
         help="Learning rate scheduler",
     )
 
-    # -------------------------------evaluation---------------------------------#
-
-    parser.add_argument(
+    evaluation = parser.add_argument_group("Evaluation")
+    evaluation.add_argument(
         "--val", action="store_true", help="If set, perform validation during training"
     )
-    parser.add_argument(
+    evaluation.add_argument(
         "--val_rate",
         default=500,
         type=int,
         help="Number of iterations between validation steps",
     )
-    parser.add_argument(
+    evaluation.add_argument(
         "--val_batch_size_multiplier",
         default=4,
         type=int,
         help="Multiplier applied to batch_size for evaluation chunking. "
-             "Can be larger than training since no gradients are computed.",
+        "Can be larger than training since no gradients are computed.",
     )
-    parser.add_argument(
+    evaluation.add_argument(
         "--val_ratio",
         default=0.25,
         type=float,
         help="Ratio of val data to be used in between epochs",
     )
-
-    parser.add_argument(
+    evaluation.add_argument(
         "--lpips_chunk_size",
         default=25,
         type=int,
         help="Number of images per chunk for LPIPS evaluation. Larger values use more GPU memory.",
     )
 
-    # -----------------------------regularizers---------------------------------#
-
-    parser.add_argument(
+    regularizers = parser.add_argument_group("Regularizers")
+    regularizers.add_argument(
         "--alpha",
         default=None,
         type=float,
         help="Initial alpha value for regularizing model parameter weights",
     )
-    parser.add_argument(
+    regularizers.add_argument(
         "--reg_ratio",
         default=0.5,
         type=float,
         help="Ratio of iterations for alpha regularizer scheduler",
     )
-    parser.add_argument(
+    regularizers.add_argument(
         "--reg",
         choices=["l1", "l2"],
         default="l1",
         help="Norm for penalizing model parameters",
     )
-    parser.add_argument(
+    regularizers.add_argument(
         "--beta",
         default=None,
         type=float,
         help="Occlusion regularization importance parameter",
     )
-    parser.add_argument(
+    regularizers.add_argument(
         "--depth_thres",
         default=0.2,
         type=float,
         help="Depth threshold to penalize sigma values",
     )
-    # -------------------------------logging------------------------------------#
 
-    parser.add_argument(
+    logging = parser.add_argument_group("Logging")
+    logging.add_argument(
         "--out_dir",
         default="../out/",
         type=str,
         help="Base directory for storing results",
     )
 
-    # -------------------------------debugging----------------------------------#
-
-    parser.add_argument(
+    debugging = parser.add_argument_group("Debugging")
+    debugging.add_argument(
         "--debug", action="store_true", help="If set, run in debug mode"
     )
 
-    # ----------------------------videorendering--------------------------------#
-
-    parser.add_argument(
+    rendering = parser.add_argument_group("Video Rendering")
+    rendering.add_argument(
         "--render_only",
         action="store_true",
         help="If set, load pretrained model to render video",
