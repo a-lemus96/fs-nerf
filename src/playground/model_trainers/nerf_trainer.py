d4a0990d47a15f9dc66110d6591c90f6e99d3b68
a-lemus96 <alejandro.lemus@cimat.mx>
2026-04-12 08:56:18 -0600

    fix occl regularization logging

--- src/playground/model_trainers/nerf_trainer.py
+++ src/playground/model_trainers/nerf_trainer.py
@@ -179,8 +179,8 @@ class NeRFModelTrainer:
                 and result.weights is not None
                 and result.weights.numel() > 0
             ):
-                occl_loss = self.occl_beta * self.occl_regularizer(result)
-                loss += occl_loss
+                occl_loss = self.occl_regularizer(result)
+                loss += self.occl_beta * occl_loss
                 if not self.debug_mode:
                     metrics["occl_loss"] = occl_loss.item()
 
